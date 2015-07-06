5787bb727e8a74fea430c52609b3adc1c5447789
Joshua "jag" Ginsberg <jag@flowtheory.net>
2015-07-06 14:06:57 -0400

    Comments for what we're doing in the tests.

--- test_project/test_app/tests.py
+++ test_project/test_app/tests.py
@@ -29,6 +29,9 @@ class HydraInitializedTestCase(TestCase):
                                                   created_by=self.user)
 
     def test_simple_model_default_branch(self):
+        # A simple insert/update/delete confirming that everything works as
+        # normal.
+
         reader_obj = Reader.objects.create(name='Book Worm',
                                            email='bookworm@example.com')
         raw_reader_obj = hydra.HydraReader.objects.get(
@@ -59,6 +62,7 @@ class HydraInitializedTestCase(TestCase):
         )
 
     def test_simple_model_custom_branch(self):
+        # Start by creating a reader in the default branch
         reader_obj = Reader.objects.create(name='Book Worm',
                                            email='bookworm@example.com')
         reader_pk = reader_obj.pk
@@ -71,7 +75,10 @@ class HydraInitializedTestCase(TestCase):
         self.assertEqual(default_raw_reader_obj.name, reader_obj.name)
         self.assertEqual(default_raw_reader_obj.email, reader_obj.email)
 
+        # Now update that reader in the test branch and confirm that a modified
+        # copy was made in the raw table
         activate_branch(self.branch)
+        reader_obj = Reader.objects.get(pk=reader_pk)
         reader_obj.name = 'Big Worm'
         reader_obj.email = 'bigworm@example.com'
         reader_obj.save()
@@ -90,6 +97,10 @@ class HydraInitializedTestCase(TestCase):
         self.assertEqual(branch_raw_reader_obj.name, reader_obj.name)
         self.assertEqual(branch_raw_reader_obj.email, reader_obj.email)
 
+        # Now to test that changes aren't reflected in the default branch yet
+        # We will delete the existing one and create a new one - both in the
+        # branch - and then deactivate the branch to ensure that the changes
+        # do not appear in the default branch
         reader_obj.delete()
         branch_reader_obj = Reader.objects.create(name='Little Tugger',
                                                   email='tugger@example.com')
@@ -102,6 +113,8 @@ class HydraInitializedTestCase(TestCase):
                           pk=branch_reader_obj.pk)
         reader_obj = Reader.objects.get(pk=reader_pk)
 
+        # Now let's make sure that the changes are still reflected in the
+        # test branch
         activate_branch(self.branch)
         branch_raw_reader_obj = hydra.HydraReader.objects.get(
             _branch_name=self.branch.branch_name,
